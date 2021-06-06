82bd520451c592dce4fca07141c7ed6eba082a84
Satoshi Nakamura <snakamura@asteria.com>
2021-06-06 18:11:01 +0900

    Support adding wind

--- src/environment.py
+++ src/environment.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional, cast
 from position import Position
 
 class Wind:
@@ -13,12 +15,43 @@ class Wind:
     def direction(self) -> float:
         return self.__direction
 
-class Environment:
-    def __init__(self) -> None:
+class Environment(ABC):
+    @abstractmethod
+    def horizontalWind(self, position: Position) -> Wind:
         pass
 
+    @abstractmethod
+    def verticalWindVelocity(self, position: Position) -> float:
+        pass
+
+class WindWithRange:
+    def __init__(self, wind: Wind, minZ: float, maxZ: float) -> None:
+        self.__wind: Wind = wind
+        self.__minZ: float = minZ
+        self.__maxZ: float = maxZ
+
+    @property
+    def wind(self) -> Wind:
+        return self.__wind
+
+    def contains(self, position: Position) -> bool:
+        return self.__minZ <= position.z and position.z < self.__maxZ
+
+class MutableEnvironment(Environment):
+    def __init__(self) -> None:
+        self.__winds: List[WindWithRange] = []
+
+    def addWind(self, wind: Wind, minZ: float, maxZ: float) -> None:
+        self.__winds.append(WindWithRange(wind, minZ, maxZ))
+
     def horizontalWind(self, position: Position) -> Wind:
-        return Wind(0, 0)
+        wind = next(filter(lambda w: w.contains(position), self.__winds), None)
+        if wind is None:
+            return Wind(0, 0)
+        else:
+            return wind.wind
 
     def verticalWindVelocity(self, position: Position) -> float:
         return 0
+
+Environment.register(MutableEnvironment)
--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
 import math
-from environment import Environment
-from glider import Control
-from glider import Glider
+from environment import MutableEnvironment, Wind
+from glider import Control, Glider
 from position import Position
 
 def main() -> None:
-    environment = Environment()
+    environment = MutableEnvironment()
+    environment.addWind(Wind(10, 0), 80, 90)
+
     glider = Glider(Position(0, 0, 100), 0, 0, 0)
     print(glider)
     for n in range(20):
-        glider = glider.apply(Control(0.1, 0.1))
+        glider = glider.apply(Control(0, 0))
         glider = glider.step(environment)
         print(glider)
 
