d3d0b3a005a9541008c8fa5611d8da53ab057978
Satoshi Nakamura <snakamura@asteria.com>
2021-06-20 21:07:01 +0900

    Stop flying when climbing up to the max altitude

--- src/fly.py
+++ src/fly.py
@@ -1,35 +1,43 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from itertools import accumulate
 from typing import Callable, List, Optional, Tuple
 from environment import Environment, MutableEnvironment, Thermal, Wind
 from glider import Control, Glider
 from position import Position
 
 def testFly(step: Callable[[Glider], Tuple[Control, Optional[Callable[[Glider], None]]]]) -> None:
+    maxNumberOfSteps = 1000
+
     environment = MutableEnvironment()
     environment.addWind(Wind(1, 0), 100, 1000)
     environment.addThermal(Thermal(0, 0, 100, 1000, 100, 3))
 
     glider = Glider(Position(-100, 0, 300), 0, 0, 0)
 
-    gliders = fly(glider, environment, step)
+    gliders = fly(glider, environment, maxNumberOfSteps, 1000, step)
 
     for index, glider in enumerate(gliders):
         print(index, glider)
     plot(gliders)
 
-def fly(glider: Glider, environment: Environment, step: Callable[[Glider], Tuple[Control, Optional[Callable[[Glider], None]]]]) -> List[Glider]:
-    def next(glider: Glider, n: int) -> Glider:
+def fly(glider: Glider, environment: Environment, maxNumberOfSteps: int, maxAltitude: float, step: Callable[[Glider], Tuple[Control, Optional[Callable[[Glider], None]]]]) -> List[Glider]:
+    gliders: List[Glider] = []
+
+    for n in range(maxNumberOfSteps):
+        gliders.append(glider)
+
         control, next = step(glider)
         glider = glider.apply(control)
         glider = glider.step(environment)
         if next is not None:
             next(glider)
-        return glider
 
-    return list(filter(lambda glider: glider.position.z >= 0,
-        accumulate(range(100), next, initial=glider)))
+        if glider.position.z <= 0:
+            break
+        elif glider.position.z >= maxAltitude:
+            break
+
+    return gliders
 
 def plot(gliders: List[Glider]) -> None:
     fig = plt.figure(figsize=(8, 8))
